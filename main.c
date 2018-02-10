2e7d6b88d1cd66ae97e1173d920af0b46e0f17bd
Abhilash Meesala <gitlab@abhilashmeesala.com>
2018-02-10 19:58:01 +0530

    Add fibonacci example

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include "include/instruction_set.h"
 #include "include/logger.h"
 
-int main(int argc, char *argv[]) {
-    log_set_level(VERYSIMPLE_VM_LOG_DEBUG);
+void run_factorial(int n) {
     int factorial[] = {
+            //if n < 2 return 1
             VERYSIMPLE_VM_LOAD, -3,
             VERYSIMPLE_VM_ICONST, 2,
             VERYSIMPLE_VM_LT,
             VERYSIMPLE_VM_BRF, 10,
             VERYSIMPLE_VM_ICONST, 1,
             VERYSIMPLE_VM_RET,
+            //else return n * factorial(n-1)
             VERYSIMPLE_VM_LOAD, -3,
             VERYSIMPLE_VM_LOAD, -3,
             VERYSIMPLE_VM_ICONST, 1,
@@ -18,12 +19,56 @@ int main(int argc, char *argv[]) {
             VERYSIMPLE_VM_CALL, 0, 1,
             VERYSIMPLE_VM_IMUL,
             VERYSIMPLE_VM_RET,
-            VERYSIMPLE_VM_ICONST, 5,
+            //entry point: call factorial(n)
+            VERYSIMPLE_VM_ICONST, n,
             VERYSIMPLE_VM_CALL, 0, 1,
             VERYSIMPLE_VM_PRINT,
             VERYSIMPLE_VM_HALT
     };
     int size = sizeof(factorial) / sizeof(factorial[0]);
     run(factorial, size, 22);
+}
+
+void run_fibonacci(int n) {
+    int fibonacci[] = {
+            //if n == 0 return 0
+            VERYSIMPLE_VM_LOAD, -3,
+            VERYSIMPLE_VM_ICONST, 0,
+            VERYSIMPLE_VM_EQ,
+            VERYSIMPLE_VM_BRF, 10,
+            VERYSIMPLE_VM_ICONST, 0,
+            VERYSIMPLE_VM_RET,
+            //if n < 3 return 1
+            VERYSIMPLE_VM_LOAD, -3,
+            VERYSIMPLE_VM_ICONST, 3,
+            VERYSIMPLE_VM_LT,
+            VERYSIMPLE_VM_BRF, 20,
+            VERYSIMPLE_VM_ICONST, 1,
+            VERYSIMPLE_VM_RET,
+            //else return fibonacci(n-1) + fibonacci(n-2)
+            VERYSIMPLE_VM_LOAD, -3,
+            VERYSIMPLE_VM_ICONST, 1,
+            VERYSIMPLE_VM_ISUB,
+            VERYSIMPLE_VM_CALL, 0, 1,
+            VERYSIMPLE_VM_LOAD, -3,
+            VERYSIMPLE_VM_ICONST, 2,
+            VERYSIMPLE_VM_ISUB,
+            VERYSIMPLE_VM_CALL, 0, 1,
+            VERYSIMPLE_VM_IADD,
+            VERYSIMPLE_VM_RET,
+            //call fibonacci(n)
+            VERYSIMPLE_VM_ICONST, n,
+            VERYSIMPLE_VM_CALL, 0, 1,
+            VERYSIMPLE_VM_PRINT,
+            VERYSIMPLE_VM_HALT
+    };
+    int size = sizeof(fibonacci) / sizeof(fibonacci[0]);
+    run(fibonacci, size, size-7);
+}
+
+int main(int argc, char *argv[]) {
+    log_set_level(VERYSIMPLE_VM_LOG_DEBUG);
+    run_factorial(10);
+    run_fibonacci(10);
     return 0;
 }
